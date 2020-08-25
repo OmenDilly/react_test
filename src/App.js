748bf5770636adda0a0469804f12ff606a36d574
OmenDilly <59134777+OmenDilly@users.noreply.github.com>
2020-08-25 10:52:28 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { memo, useState, useCallback } from 'react'
+
+const App = () => {
+    return (
+        <div className='main__container'>
+            hello
+        </div>
+    )
+}
+
+export default memo(App)
